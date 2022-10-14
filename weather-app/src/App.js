eaf29720640403bce242a42d09d55cb702125a90
alis <ali_drn_333@hotmail.com>
2022-10-15 02:43:08 +0300

    .3

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,22 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    const key = 'd53580f84f407e33d412dca4adc2ea09'
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`,
+        )
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getApi()
+  }, [])
   return <div className="App">HELLO</div>
 }
 
